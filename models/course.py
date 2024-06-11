dffc20d50764aa613650a23ac121e5a8a8dd439f
Mariya <mariaomware@gmail.com>
2024-06-11 21:14:57 +0300

    Models: Added course model with save and all methods

---
+++ models/course.py
@@ -0,0 +1,52 @@
+from database.connection import get_db_connection
+
+class Course:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Name must be a string.")
+        if len(value) == 0:
+            raise ValueError("Name must not be empty.")
+        self._name = value
+
+    @property
+    def description(self):
+        return self._description
+    
+    @description.setter
+    def description(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Description must be a string")
+        if len(value) == 0:
+            raise ValueError("Description must not be empty")
+        self._description = value
+
+    def save(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+        INSERT INTO courses(name, description)
+        VALUES (?, ?)
+        ''', (self.name, self.description))
+        conn.commit()
+        conn.close()
+
+    @staticmethod
+    def all():
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM courses')
+        courses = cursor.fetchall()
+        conn.close()
+        return [dict(course) for course in courses]
+    
+    def __repr__(self):
+        return f'<Course {self.name}>'

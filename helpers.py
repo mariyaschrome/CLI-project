3d5a9e93574ed3a79b5d611da6c40c950b865cb3
Mariya <mariaomware@gmail.com>
2024-06-11 21:21:46 +0300

    Helpers: Added helper function for listing courses

--- helpers.py
+++ helpers.py
@@ -7,3 +7,8 @@ def list_students():
     students = Student.all()
     for student in students:
         print(f"Student ID: {student['id']}, Name: {student['name']}, Age: {student['age']}, Course: {student['course_name']}, Instructor: {student['instructor_name']}")
+
+def list_courses():
+    courses = Course.all()
+    for course in courses:
+        print(f"Course ID: {course['id']}, Name: {course['name']}, Description: {course['description']}")

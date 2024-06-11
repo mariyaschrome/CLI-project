9d87dce8120185ae0af82e8796a47ad45543424f
Mariya <mariaomware@gmail.com>
2024-06-11 21:13:45 +0300

    Models: Added Student model with save and all methods

---
+++ models/student.py
@@ -0,0 +1,77 @@
+from database.connection import get_db_connection
+from .enrollment import Enrollment
+
+class Student:
+    def __init__(self, name, age, course_name, instructor_name):
+        self.name = name
+        self.age = age
+        self.course_name = course_name
+        self.instructor_name = instructor_name
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Name must be a string.")
+        if len(value) == 0:
+            raise ValueError("Name must not be empty.")
+        self._name = value
+
+    @property
+    def age(self):
+        return self._age
+    
+    @age.setter
+    def age(self, value):
+        if not isinstance(value, int):
+            raise ValueError("Age must be an integer")
+        self._age = value
+
+    @property
+    def course_name(self):
+        return self._course_name
+    
+    @course_name.setter
+    def course_name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Course name must be a string.")
+        if len(value) == 0:
+            raise ValueError("Course name must not be empty.")
+        self._course_name = value
+
+    @property
+    def instructor_name(self):
+        return self._instructor_name
+    
+    @instructor_name.setter
+    def instructor_name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Instructor name must be a string.")
+        if len(value) == 0:
+            raise ValueError("Instructor name must not be empty.")
+        self._instructor_name = value
+
+    def save(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO students (name, age, course_name, instructor_name) 
+            VALUES (?, ?, ?, ?) 
+            ''', (self.name, self.age, self.course_name, self.instructor_name))
+        conn.commit()
+        conn.close()
+
+    @staticmethod
+    def all():
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM students')
+        students = cursor.fetchall()
+        conn.close()
+        return students
+    
+    def __repr__(self):
+        return f'<Student {self.name}>'

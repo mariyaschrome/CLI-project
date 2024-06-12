ff550f8d46867a46b70df2b7b581ab2285882a51
Mariya <mariaomware@gmail.com>
2024-06-12 16:41:44 +0300

    App: Updated choices

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from database.setup import create_tables
 from helpers import (
     list_students, list_courses, list_instructors, list_enrollments,
-    add_student, add_course, add_instructor, enroll_student
+    add_student, add_course, add_instructor, enroll_student,
+    update_student, update_course, update_instructor, update_enrollment,
+    delete_student, delete_course, delete_instructor, delete_enrollment
 )
 
 def main():
@@ -16,9 +18,18 @@ def main():
         6. Add course
         7. Add instructor
         8. Enroll student
-        9. Exit
+        9. Update student
+        10. Update course
+        11. Update instructor
+        12. Update enrollment
+        13. Delete student
+        14. Delete course
+        15. Delete instructor
+        16. Delete enrollment
+        17. Clear database
+        18. Exit
         """)
-        choice = input("Enter your choice:")
+        choice = input("Enter your choice: ")
         if choice == '1':
             list_students()
         elif choice == '2':
@@ -36,11 +47,29 @@ def main():
         elif choice == '8':
             enroll_student()
         elif choice == '9':
-            print("Goodbye, thanks for using enrollment manager!")
+            update_student()
+        elif choice == '10':
+            update_course()
+        elif choice == '11':
+            update_instructor()
+        elif choice == '12':
+            update_enrollment()
+        elif choice == '13':
+            delete_student()
+        elif choice == '14':
+            delete_course()
+        elif choice == '15':
+            delete_instructor()
+        elif choice == '16':
+            delete_enrollment()
+        elif choice == '17':
+            clear_database()
+            print("Database cleared.")
+        elif choice == '18':
+            print("Goodbye, thanks for using us!")
             break
         else:
             print("Invalid choice")
-            
 
 if __name__ == "__main__":
     main()
